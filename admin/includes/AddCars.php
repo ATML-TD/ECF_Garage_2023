c4d1a5496cac5edc2eff8c92a353b78de302fae2
ATML-TD <alicia.trandien@gmail.com>
2023-06-19 15:27:01 +0200

    Admin add cars function

--- admin/includes/AddCars.php
+++ admin/includes/AddCars.php
@@ -2,6 +2,7 @@
 session_start();
 error_reporting(0);
 include('../includes/Config.php');
+
 if (strlen($_SESSION['alogin']) == 0) {
 	header('location:index.php');
 } else {
@@ -55,6 +56,8 @@ if (strlen($_SESSION['alogin']) == 0) {
 
 
 ?>
+
+
 	<!DOCTYPE html>
 	<html lang="fr">
 
@@ -67,132 +70,151 @@ if (strlen($_SESSION['alogin']) == 0) {
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		<link rel="stylesheet" href="../assets/css/style.css">
+		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css">
+		<script src="//code.jquery.com/jquery-3.7.0.js"></script>
+		<script src="//code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 	</head>
 
-
 	<body>
-		<div class="ts-main-content">
-			<div class="content-wrapper">
-				<div class="container-fluid">
 
-					<div class="row">
-						<div class="col-md-12">
+		<div class="navbar">
+			<a class="homepage" href="../index.php"><i class="fa fa-fw fa-home"></i> Retour au site</a>
+			<a href="includes/Logout.php"><i class="fa fa-fw fa-user"></i> Se déconnecter</a>
+		</div>
 
-							<h2 class="page-title">Ajouter une voiture</h2>
-
-							<div class="row">
-								<div class="col-md-12">
-									<div class="panel panel-default">
-
-										<div class="panel-body">
-											<form method="post" class="form-horizontal" enctype="multipart/form-data">
-												<div class="form-group">
-													<label class="col-sm-2 control-label">Nom du véhicule<span style="color:red">*</span></label>
-													<div class="col-sm-4">
-														<input type="text" name="vehicletitle" class="form-control" required>
-													</div>
-													<label class="col-sm-2 control-label">Nom du véhicule<span style="color:red">*</span></label>
-													<div class="col-sm-4">
-														<input type="text" name="vehicletitle" class="form-control" required>
-													</div>
-													<label class="col-sm-2 control-label">Prix du véhicule<span style="color:red">*</span></label>
-													<div class="col-sm-4">
-														<input type="text" name="vehicletitle" class="form-control" required>
-													</div>
-													<label class="col-sm-2 control-label">Année du véhicule<span style="color:red">*</span></label>
-													<div class="col-sm-4">
-														<input type="text" name="vehicletitle" class="form-control" required>
-													</div>
-													<label class="col-sm-2 control-label">Kilométrage du véhicule<span style="color:red">*</span></label>
-													<div class="col-sm-4">
-														<input type="text" name="vehicletitle" class="form-control" required>
-													</div>
-													<div class="form-group">
-														<label class="col-sm-2 control-label">Vehical Overview<span style="color:red">*</span></label>
-														<div class="col-sm-10">
-															<textarea class="form-control" name="vehicalorcview" rows="3" required></textarea>
-														</div>
-													</div>
-													<label class="col-sm-2 control-label">Nombre de sièges<span style="color:red">*</span></label>
-													<div class="col-sm-4">
-														<input type="text" name="vehicletitle" class="form-control" required>
-													</div>
-													<div class="form-group">
-														<label class="col-sm-2 control-label">Select Fuel Type<span style="color:red">*</span></label>
-														<div class="col-sm-4">
-															<select class="selectpicker" name="fueltype" required>
-																<option value=""> Select </option>
-																<option value="Petrol">Petrol</option>
-																<option value="Diesel">Diesel</option>
-																<option value="CNG">CNG</option>
-															</select>
-														</div>
-													</div>
-
-													<div class="form-group">
-														<div class="col-sm-12">
-															<h4><b>Ajouter des images</b></h4>
-														</div>
-													</div>
+		<div class="sidenav">
+			<div class="dropdown">
+				<button class="dropdown-btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+					<i class="fa fa-car"></i> Gérer les voitures
+				</button>
+				<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+					<a class="dropdown-item" href="includes/AddCars.php">Ajouter</a>
+					<a class="dropdown-item" href="#">Supprimer</a>
+				</div>
+			</div>
 
 
-													<div class="form-group">
-														<div class="col-sm-4">
-															Image 1 <span style="color:red">*</span><input type="file" name="img1" required>
-														</div>
-														<div class="col-sm-4">
-															Image 2<span style="color:red">*</span><input type="file" name="img2" required>
-														</div>
-														<div class="col-sm-4">
-															Image 3<span style="color:red">*</span><input type="file" name="img3" required>
-														</div>
-													</div>
+			<a href="#"><i class="fa fa-pen"></i> Validation commentaires</a>
+			<a href="#"><i class="fa fa-table"></i> Modifier les horaires</a>
+			<a href="#"><i class="fa fa-user"></i> Ajouter des employés</a>
+		</div>
 
+		<div class=main>
+			<div class="ts-main-content">
+				<div class="content-wrapper">
+					<div class="container-fluid">
 
-													<div class="form-group">
-														<div class="col-sm-4">
-															Image 4<span style="color:red">*</span><input type="file" name="img4" required>
-														</div>
-														
+						<div class="row">
+							<div class="col-md-12">
 
-													</div>
-												</div>
-											</form>
-										</div>
-									</div>
-								</div>
+								<h2 class="page-title">Ajouter une voiture</h2>
 
 								<div class="row">
 									<div class="col-md-12">
 										<div class="panel panel-default">
-											<div class="panel-heading">Accessories</div>
+
 											<div class="panel-body">
+												<form method="post" class="form-horizontal" enctype="multipart/form-data">
+													<div class="form-group">
+														<label class="col-sm-2 control-label">Nom du véhicule<span style="color:red">*</span></label>
+														<div class="col-sm-4">
+															<input type="text" name="carsname" class="form-control" required>
+														</div>
+														<label class="col-sm-2 control-label">Prix du véhicule<span style="color:red">*</span></label>
+														<div class="col-sm-4">
+															<input type="text" name="carsprice" class="form-control" required>
+														</div>
+														<label class="col-sm-2 control-label">Année du véhicule<span style="color:red">*</span></label>
+														<div class="col-sm-4">
+															<input type="text" name="carsyear" class="form-control" required>
+														</div>
+														<label class="col-sm-2 control-label">Kilométrage du véhicule<span style="color:red">*</span></label>
+														<div class="col-sm-4">
+															<input type="text" name="carskm" class="form-control" required>
+														</div>
+														<div class="form-group">
+															<label class="col-sm-2 control-label">Détails du véhicule<span style="color:red">*</span></label>
+															<div class="col-sm-10">
+																<textarea class="form-control" name="carsdescription" rows="3" required></textarea>
+															</div>
+														</div>
+														<label class="col-sm-2 control-label">Nombre de sièges<span style="color:red">*</span></label>
+														<div class="col-sm-4">
+															<input type="text" name="carsseat" class="form-control" required>
+														</div>
+														<div class="form-group">
+															<label class="col-sm-2 control-label">Type de véhicule<span style="color:red">*</span></label>
+															<div class="col-sm-4">
+																<select class="selectpicker" name="carstype" required>
+																	<option value=""> Selectionner </option>
+																	<option value="Essence">Essence</option>
+																	<option value="Electrique">Electrique</option>
+																	<option value="Hybride">Hybride</option>
+																</select>
+															</div>
+														</div>
+
+														<div class="form-group">
+															<div class="col-sm-12">
+																<h4><b>Ajouter des images</b></h4>
+															</div>
+														</div>
 
 
-												<div class="form-group">
-													<div class="col-sm-3">
-														<div class="checkbox checkbox-inline">
-															<input type="checkbox" id="airconditioner" name="airconditioner" value="1">
-															<label for="airconditioner"> Air Conditioner </label>
+														<div class="form-group">
+															<div class="col-sm-4">
+																Image 1 <span style="color:red">*</span><input type="file" name="carsimage1" required>
+															</div>
+															<div class="col-sm-4">
+																Image 2<span style="color:red">*</span><input type="file" name="carsimage2" required>
+															</div>
+															<div class="col-sm-4">
+																Image 3<span style="color:red">*</span><input type="file" name="carsimage3" required>
+															</div>
+															<div class="col-sm-4">
+																Image 4<span style="color:red">*</span><input type="file" name="carsimage4" required>
+															</div>
 														</div>
 													</div>
-													<div class="col-sm-3">
-														<div class="checkbox checkbox-inline">
-															<input type="checkbox" id="powerdoorlocks" name="powerdoorlocks" value="1">
-															<label for="powerdoorlocks"> Power Door Locks </label>
+
+													<div class="row">
+														<div class="col-md-12">
+															<div class="panel panel-default">
+																<div class="panel-heading">Accessoires</div>
+																<div class="panel-body">
+
+
+																	<div class="form-group">
+																		<div class="col-sm-3">
+																			<div class="checkbox checkbox-inline">
+																				<input type="checkbox" id="climatisation" name="carsac" value="1">
+																				<label for="climatisation"> Climatisation </label>
+																			</div>
+																		</div>
+																		<div class="col-sm-3">
+																			<div class="checkbox checkbox-inline">
+																				<input type="checkbox" id="bluetooth" name="bluetooth" value="1">
+																				<label for="bluetooth"> Bluetooth </label>
+																			</div>
+																		</div>
+																		<div class="col-sm-3">
+																			<div class="checkbox checkbox-inline">
+																				<input type="checkbox" id="autolight" name="autolight" value="1">
+																				<label for="autolight"> Feux automatique </label>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</div>
 														</div>
 													</div>
-													<div class="col-sm-3">
-														<div class="checkbox checkbox-inline">
-															<input type="checkbox" id="antilockbrakingsys" name="antilockbrakingsys" value="1">
-															<label for="antilockbrakingsys"> AntiLock Braking System </label>
+													<div class="form-group">
+														<div class="col-sm-8 col-sm-offset-2">
+															<button class="btn btn-default" type="reset">Annuler</button>
+															<button class="btn btn-danger" name="submit" type="submit">Ajouter le véhicule</button>
 														</div>
 													</div>
-													<div class="checkbox checkbox-inline">
-														<input type="checkbox" id="brakeassist" name="brakeassist" value="1">
-														<label for="brakeassist"> Brake Assist </label>
-													</div>
-												</div>
+												</form>
 											</div>
 										</div>
 									</div>
@@ -203,6 +225,9 @@ if (strlen($_SESSION['alogin']) == 0) {
 				</div>
 			</div>
 		</div>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 	</body>
 
 	</html>
--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,3 @@
-<?php
-session_start();
-error_reporting(0);
-
-include("includes/AddCars.php");
-
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -17,33 +9,49 @@ include("includes/AddCars.php");
     <link rel="shortcut icon" href="../assets/images/icon.jpg" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="//code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 </head>
 
 <body>
-    <header class="admin-header text-paragraph flux">
-        <img class="logo" src="../assets/images/logo.jpg" alt="Logo Garage V Parrot">
-        <button type="submit" id="logout" class="btn btn-danger btn-lg pull-right ">Deconnexion</button>
-    </header>
-    <div class="ts-main-content">
-        <nav class="ts-sidebar">
-            <ul class="ts-sidebar-menu">
 
-                <li><a href="#"><i class="fa fa-car"></i>Voitures d'occasion</a>
-                    <ul>
-                        <li><a href="AddCars.php">Ajouter une voiture</a></li>
-                        <li><a href="manage-vehicles.php">Supprimer une voiture</a></li>
-                    </ul>
-                </li>
-
-                <li><a href="testimonials.php"><i class="fa fa-table"></i>Validation commentaires</a></li>
-                <li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i>Modifier les horaires</a></li>
-                <li><a href="manage-subscribers.php"><i class="fa fa-table"></i>Ajouter des employés</a></li>
-
-            </ul>
-        </nav>
-
+    <div class="navbar">
+        <a class="homepage" href="../index.php"><i class="fa fa-fw fa-home"></i> Retour au site</a>
+        <a href="includes/Logout.php"><i class="fa fa-fw fa-user"></i> Se déconnecter</a>
     </div>
+
+    <div class="sidenav">
+        <div class="dropdown">
+            <button class="dropdown-btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fa fa-car"></i> Gérer les voitures
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="includes/AddCars.php">Ajouter</a>
+                <a class="dropdown-item" href="#">Supprimer</a>
+            </div>
+        </div>
+
+
+        <a href="#"><i class="fa fa-pen"></i> Validation commentaires</a>
+        <a href="#"><i class="fa fa-table"></i> Modifier les horaires</a>
+        <a href="#"><i class="fa fa-user"></i> Ajouter des employés</a>
+    </div>
+
+    <div class="main">
+        <h2>Bienvenue Monsieur Parrot</h2>
+        <p>Ici vous pouvez gérer les voitures d'occasion,</p>
+        <p>les horaires du magasin</p>
+        <p>ainsi que vos employés</p>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
+
+
 
 </html>
