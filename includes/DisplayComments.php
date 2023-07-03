7a1a920ceb3d088dcdfe611f8c34ec17f72353c3
ATML-TD <alicia.trandien@gmail.com>
2023-07-03 09:31:16 +0200

    Fix Display comments

--- includes/DisplayComments.php
+++ includes/DisplayComments.php
@@ -10,27 +10,40 @@ $result = $query->fetch();
 <section class="flux">
     <h3>Votre avis compte!</h3>
     <!-- Display comments and rating -->
-    <div class="card">
-        <div class="card-header">
-            <h5><?php echo htmlentities($result["usermail"]); ?></h5>
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <p><?php echo htmlentities($result["comment"]); ?></p>
-                    </div>
-                    <div class="col" id="displaystars">
-                        <script>
-                            var html = '';
-                            var starrating = "<?php print($result["userrating"]); ?>;"
-                            starrating = parseInt(starrating)
-                            for (var star = 1; star <= Number(starrating); star++) {
-                                console.log(starrating)
-                                html += '<i class="fa-regular fa-star" style="color: #f6d32d;"></i>';
-                            }
-                            document.getElementById("displaystars").innerHTML = html;
-                        </script>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    <table class="table table-striped table-hover">
+        <tr>
+            <th>Client</th>
+            <th>Commentaire</th>
+            <th>Note</th>
+        </tr>
+
+        <?php
+        ini_set('display_errors', 'on');
+        error_reporting(E_ALL);
+        include('includes/Config.php');
+        $sql = "SELECT tabcomment.id, tabcomment.usermail, tabcomment.comment, tabcomment.status, tabcomment.userrating from tabcomment where status=1";
+        $query = $pdo->prepare($sql);
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_OBJ);
+        $cnt = 1;
+        if ($query->rowCount() > 0) {
+            foreach ($results as $result) { ?>
+                <tr>
+                    <td><?php echo htmlentities($result->usermail); ?></td>
+                    <td><?php echo htmlentities($result->comment); ?></td>
+                    <td>
+                        <?php
+                        $stars = $result->userrating;
+                        $starhtml = '<i class="fa-regular fa-star" style="color: #f6d32d;"></i>';
+                        echo str_repeat($starhtml, $stars);
+
+                        ?>
+                    </td>
+                </tr>
+        <?php $cnt = $cnt + 1;
+            }
+        } ?>
+    </table>
+
+</section>
