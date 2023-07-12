f74673ca5c48ab40e03a474f64b896d4ef82ae7c
ATML-TD <alicia.trandien@gmail.com>
2023-07-12 16:59:16 +0200

    fix database

--- includes/Config.php
+++ includes/Config.php
@@ -1,9 +1,26 @@
 <?php 
-    // DB credentials.
-    $host = 'localhost';
+
+//Heroku
+
+if(getenv('JAWSDB_URL') !== false){
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+} else {
+    // DB credentials local
+    $host = '127.0.0.1';
     $username = 'root';
     $password = '';
     $dbname = 'garageparrot';
+}
+
+
+
+
     
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
