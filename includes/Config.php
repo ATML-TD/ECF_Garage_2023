855198ce215766b796620ce5343793f8b19e2469
ATML-TD <alicia.trandien@gmail.com>
2023-07-12 20:56:11 +0200

    Adapt for heroku

--- includes/Config.php
+++ includes/Config.php
@@ -1,10 +1,23 @@
 <?php 
-    // DB credentials.
-    $host = 'localhost';
+<?php 
+
+//Heroku
+
+if(getenv('JAWSDB_URL') !== false){
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+
+    $host = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $dbname = ltrim($dbparts['path'],'/');
+} else {
+    // DB credentials local
+    $host = '127.0.0.1';
     $username = 'root';
     $password = '';
     $dbname = 'garageparrot';
-    
+}
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         
