1c7f4f6d9d04af5c51d2ed3440c050769f65f2f4
ATML-TD <alicia.trandien@gmail.com>
2023-07-14 11:55:19 +0200

    Change validator

--- CarsOverview.php
+++ CarsOverview.php
@@ -182,7 +182,6 @@ error_reporting(E_ALL);
     <!--/Login-Form -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="assets/js/validator.min.js"></script>
     <script src="assets/js/main.js"></script>
 
 
