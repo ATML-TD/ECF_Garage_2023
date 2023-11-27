5a4b6a793877b4e3e62848d2019e7be91a7a44e4
ATML-TD <alicia.trandien@gmail.com>
2023-11-27 14:40:44 +0100

    return button style

--- admin/includes/EditOpeningForm.php
+++ admin/includes/EditOpeningForm.php
@@ -9,7 +9,7 @@
     <link rel="shortcut icon" href="../assets/images/icon.jpg" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css">
     <script src="//code.jquery.com/jquery-3.7.0.js"></script>
     <script src="//code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
@@ -19,8 +19,8 @@
 <body>
     <div class="container flux">
         <div class="navbar">
-			<a class="homepage" href="../index.php"><i class="fa fa-fw fa-home"></i> Retour</a>
-		</div>
+            <a class="home" href="../index.php"><i class="fa fa-fw fa-home"></i> Retour</a>
+        </div>
         <div class="row">
             <div class="col-md-12">
                 <h2 class="title">Modifier les horaires</h2>
